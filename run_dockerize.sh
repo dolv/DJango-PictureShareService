210f875aedb3249efa4d989ae13615cd393d86a8
DUDCHENKO Oleksanr <dolv@ukr.net>
2018-06-05 16:30:37 +0300

    updated README; fixed collectstatic; refactored entrypoint script

--- run_dockerize.sh
+++ run_dockerize.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 # set -x
-source ${APP_VIRTUALENV_PATH}/bin/activate
-which pip3
-which python3
-pip3 freeze
-python3 manage.py migrate
-python3 manage.py collectstatic
 dockerize \
 -template ${APP_DIR}/settings.py.tmpl:${APP_DIR}/PictureShareService/settings.py \
 -template ${APP_DIR}/uwsgi.ini.tmpl:${APP_DIR}/uwsgi.ini \
