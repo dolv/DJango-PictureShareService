86b6d8fecba2726968977a2de574026dfb7a7ced
Oleksandr DUDCHENKO <oleksandr.d@openweb.com>
2022-02-17 16:24:43 +0200

    upgrade modules

--- PictureShareService/settings.py
+++ PictureShareService/settings.py
@@ -177,7 +177,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = config.get_value(
     'CORS_ORIGIN_WHITELIST',
-    ('localhost:4200',)
+    ('http://localhost:4200',)
 )
 
 ALLOWED_HOSTS = config.get_value(
